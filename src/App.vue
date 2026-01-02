27acec4ef2fc0121bfa0e19015aa7700e5060a4a
Alex Costantino <166677664+acitd@users.noreply.github.com>
2026-01-02 19:24:58 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> and <a href="https://www.stylezero.org/" target="_blank" rel="noopener">stylezero.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped>
+	@import'./assets/stylezero.css';
+</style>
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import stylezero from 'stylezero'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    stylezero(),
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
