0152aeda9e0c9e7145e485dc5860fc26b4716a5f
Alex Costantino <166677664+acitd@users.noreply.github.com>
2026-01-02 20:18:10 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1 class="0{color:#42b883}">You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> and <a href="https://www.stylezero.org/" target="_blank" rel="noopener">stylezero.org</a> to read the
     documentation
