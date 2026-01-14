140b5ea142685228739a1b247a0793117a4e0839
Alex Costantino <166677664+acitd@users.noreply.github.com>
2026-01-14 20:56:16 +0200

    Create Home.vue

---
+++ src/page/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+	
+</script>
+<template>
+	<h1 class="0{color:#42b883}">You did it!</h1>
+	<p>
+		Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> and <a href="https://www.stylezero.org/" target="_blank" rel="noopener">stylezero.org</a> to read the documentation
+	</p>
+</template>
