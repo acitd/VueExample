6cd7280dff20921201d7d2b2af27ca49d05dc935
Alex Costantino <166677664+acitd@users.noreply.github.com>
2026-01-14 20:59:57 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  resolve:{
+    alias:{
+      '@':fileURLToPath(new URL('./src',import.meta.url))
     },
   },
 })
