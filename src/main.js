22d7668af5ee32363ca455777187e7782224109d
Alex Costantino <166677664+acitd@users.noreply.github.com>
2026-01-14 20:59:10 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+// Ingore this file
+import{createApp}from'vue';
+import App from'@/App.vue';
+import router from'@/router/index.js';
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const app=createApp(App);
+app.use(router);
+app.mount(document.body);
