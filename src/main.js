b8e9547462e9b23a9829d764b393e237162eb2e2
Alex Costantino <166677664+acitd@users.noreply.github.com>
2026-01-14 21:24:37 +0200

    Delete src/main.js

--- src/main.js
+++
@@ -1,8 +0,0 @@
-// Ingore this file
-import{createApp}from'vue';
-import App from'@/App.vue';
-import router from'@/router/index.js';
-
-const app=createApp(App);
-app.use(router);
-app.mount(document.body);
