2d13ef18fc9ffecceeabe2cbef69ed5d8f951e86
Alex Costantino <166677664+acitd@users.noreply.github.com>
2026-01-14 20:54:57 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import{createRouter,createWebHistory}from'vue-router';
+import Home from'@/page/Home.vue';
+import About from'@/page/About.vue';
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+
+const router=createRouter({
+  history:createWebHistory(import.meta.env.BASE_URL),
+  routes:[
+  	{path:'/',component:Home},
+  	{path:'/about',component:About}
+  ],
 })
 
-export default router
+export default router;
