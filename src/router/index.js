e38df589522d35789f60f2f1b9578689ecf7e325
Alex Costantino <166677664+acitd@users.noreply.github.com>
2026-01-15 01:58:48 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import About from'@/page/About.vue';
 
 
 const router=createRouter({
-  createWebHistory((()=>{const r=[];for(const s of import.meta.url.substr(location.origin.length+1).split('/')){if(s==='src'||s==='assets')break;r.push(s)}return'/'+r.join('/')})()),
+  createWebHistory((()=>{const r=[];for(const s of import.meta.url.substr(location.origin.length+1).split('/')){if(s==='assets'||s==='src')break;r.push(s)}return'/'+r.join('/')})()),
   routes:[
   	{path:'/',component:Home},
   	{path:'/about',component:About}
