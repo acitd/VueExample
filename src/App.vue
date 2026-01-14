a869e33e8713c248aa1c01fabdc5e8feee0bcd3d
Alex Costantino <166677664+acitd@users.noreply.github.com>
2026-01-14 20:53:36 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
-<script setup></script>
+<script setup>
+	import{RouterLink,RouterView}from'vue-router';
+	import AppButton from'@/component/AppButton.vue';
+	import app from'@/app.js';
+</script>
 
 <template>
-  <h1 class="0{color:#42b883}">You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> and <a href="https://www.stylezero.org/" target="_blank" rel="noopener">stylezero.org</a> to read the
-    documentation
-  </p>
+	<RouterLink to="/" class="0{padding:5px}">Home</RouterLink>
+	<RouterLink to="/about" class="0{padding:5px}">About</RouterLink>
+	<RouterView/>
 </template>
 
-<style scoped>
-	@import'./assets/stylezero.css';
-</style>
+<style>@import'/assets/stylezero/global.css';</style>
