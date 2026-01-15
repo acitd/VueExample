4730e0292bb3416f2c0b5f6431e3c746d2df5ae1
Alex Costantino <166677664+acitd@users.noreply.github.com>
2026-01-15 02:01:36 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,12 @@ import{createRouter,createWebHistory}from'vue-router';
 import Home from'@/page/Home.vue';
 import About from'@/page/About.vue';
 
-
 const router=createRouter({
-  createWebHistory((()=>{const r=[];for(const s of import.meta.url.substr(location.origin.length+1).split('/')){if(s==='assets'||s==='src')break;r.push(s)}return'/'+r.join('/')})()),
-  routes:[
-  	{path:'/',component:Home},
-  	{path:'/about',component:About}
-  ],
+	history:createWebHistory((()=>{const r=[];for(const s of import.meta.url.substr(location.origin.length+1).split('/')){if(s==='assets'||s==='src')break;r.push(s)}return'/'+r.join('/')})()),
+	routes:[
+		{path:'/',component:Home},
+		{path:'/about',component:About}
+	],
 })
 
 export default router;
